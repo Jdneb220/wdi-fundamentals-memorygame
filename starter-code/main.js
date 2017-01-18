a8e1e2309a60384b72bb040fd3a07cd3b8fd4f14
Benjamin de Jesus <benjamindejesus@Benjamins-MacBook-Pro.local>
2017-01-17 18:52:53 -1000

    Unit 10 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,7 +4,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if (cardTwo == cardFour)
 	alert ("You found a match!");
 else
 	alert ("Sorry, try again.");
+*/
+
+
+
+function createCards(){
+	var gameBoard = document.getElementById('game-board');
+	for (var i = 0; i < 4; i++){
+		var card = document.createElement('div');
+		card.setAttribute('class','card');
+		gameBoard.appendChild(card);
+	}
+}
